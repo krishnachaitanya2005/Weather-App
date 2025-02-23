34d26f6f2441a4cca84cf255f4ce24cb90ee2a4d
krishnachaitanya2005 <chaitanya.jgs@gmail.com>
2025-02-23 22:21:50 +0530

    first commit before completion of project

---
+++ major-project-react/src/App.jsx
@@ -0,0 +1,14 @@
+import SearchBox from "./searchBox";
+import InfoBox from "./infoBox";
+// import "./App.css";
+
+function App() {
+	return (
+		<>
+			<SearchBox />
+			<InfoBox />
+		</>
+	);
+}
+
+export default App;
